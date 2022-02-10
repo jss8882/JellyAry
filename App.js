6136d83dd4a173ee1fb97b826013093a7efb1986
sangsuJang <sangsujang@sangsuJangui-MacBookAir.local>
2022-02-10 23:06:23 +0900

    function component

--- App.js
+++ App.js
@@ -72,58 +72,62 @@ export default function App() {
   const ScheduleName = styled.Text`
     margin-left: 10px;
     align-self: center;
-
   `
 
+  const Goal = ({title, todos}) =>{
 
+
+    return(
+      <>
+        <GoalContainer>
+          <GoalCategory>{title}</GoalCategory>
+          <Circle>
+            <AddIcon>
+              +
+            </AddIcon>
+          </Circle>
+        </GoalContainer>
+        {
+          //javascript code
+          todos.map( (todo)=>(
+              <TodoContainer>
+                <CompletedBox/>
+                <ScheduleName>{todo}</ScheduleName>
+
+              </TodoContainer>
+          ))
+        }
+      </>
+    )
+  }
+
+  //배열, 어플(컴포넌트)의 상태(State)
+  //함수형 컴포넌트
+
+  //숫자 1,2,3
+  const myNumber = 1;
+
+  //배열(Array) []
+  const todos = ["빨래하기", "치킨먹기","혜미니 과외"];
+  const myArray = []
+
+
+  //함수(function) ()=>{}  ()=>()
+  //함수는 인풋(paramter), 프로세싱(중괄호 안에 있는 코드), 아웃풋 (return안에)
+
+  // const myFunction = (parameter) => {
+  //   code
+  // }
 
 
   //return안에 있는거는 보여지는 코드
   //IOS의 경우에는
   return (
       <Container>
-        <GoalContainer>
-          <GoalCategory>✔️🔥🗒</GoalCategory>
-          <Circle>
-            <AddIcon>
-              +
-            </AddIcon>
-          </Circle>
-        </GoalContainer>
 
-        <TodoContainer>
-          <CompletedBox/>
-          <ScheduleName>빨래하기</ScheduleName>
-        </TodoContainer>
-
-        <TodoContainer>
-          <IncompletedBox/>
-          <ScheduleName>치킨먹기</ScheduleName>
-        </TodoContainer>
-
-        <GoalContainer>
-          <GoalCategory>혜미니👻</GoalCategory>
-          <Circle>
-            <AddIcon>
-              +
-            </AddIcon>
-          </Circle>
-        </GoalContainer>
-
-        <TodoContainer>
-          <CompletedBox/>
-          <ScheduleName>공덕가기이잉이ㅣ</ScheduleName>
-        </TodoContainer>
-
-        <TodoContainer>
-          <IncompletedBox/>
-          <ScheduleName>해장국 뇸뇸</ScheduleName>
-        </TodoContainer>
-
-        <TodoContainer>
-          <IncompletedBox/>
-          <ScheduleName>순대국 뇸뇸</ScheduleName>
-        </TodoContainer>
+        <Goal title={"✔️🔥🗒"} todos={["빨래하기", "치킨먹기", "혜미니 영통"]}/>
+        <Goal title={"혜미니👻"} todos={["공덕가기이이이ㅣ", "해장국 뇸뇸", "순대국 뇸뇸"]}/>
+        <Goal title={"개인공부👻"} todos={["김브래인씨ㅣ", "논문"]}/>
 
       </Container>
   );
