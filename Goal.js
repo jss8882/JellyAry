6f9be8bf76c065a20323ed87e122a90414bf0373
sangsuJang <sangsujang@sangsuJangui-MacBookAir.local>
2022-04-20 21:36:43 +0900

    icon and delete todo

--- Goal.js
+++ Goal.js
@@ -1,6 +1,8 @@
 import React, {useState, useEffect} from "react";
 import { Pressable, TextInput } from "react-native";
+import BouncyCheckbox from "react-native-bouncy-checkbox";
 import styled from "styled-components/native";
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 
 
@@ -51,6 +53,9 @@ const Goal = ({title}) => {
 
   const TodoContainer = styled.View`
     flex-direction: row;
+    width: 50%;
+    justify-content: space-between;
+    align-items: center;
     margin-top: 10px;
   `;
 
@@ -69,7 +74,6 @@ const Goal = ({title}) => {
   `;
 
   const ScheduleName = styled.Text`
-    margin-left: 10px;
     align-self: center;
   `;
 
@@ -80,15 +84,26 @@ const Goal = ({title}) => {
   //타이핑중인 아직 추가되지는 않은 일정
   const [newTodo, setNewTodo] = useState("")
 
+  //TextInput이 보여지는 상태인지를 저장하는 상태
+  const [inputShown,setInputShown] = useState(false)
+
   useEffect(()=>{
     console.log("new todo: ", newTodo)
   },[newTodo])
+
+  useEffect(()=>{
+    console.log(todos)
+  },[todos])
 
 
   return (
     <>
       <Pressable
-        
+        // onPress={()=>setInputShown(true)}
+        // !:NOT연산자
+        //false=>!false  Not false => true
+        //true => !true not true => false
+        onPress={()=>setInputShown(prev=>!prev)}
       >
         <GoalContainer>
           <GoalCategory>{title}</GoalCategory>
@@ -100,41 +115,63 @@ const Goal = ({title}) => {
       {
         //javascript code
         //배열크기만큼 반복하는 코드 (todos에 해야할일이 3개가 있으면 3번반복, 2개 있으면 2번 반복)
-        todos.map((todo) => (
-          <Pressable
-            onPress={() => {
-              console.log(`${todo} is completed`);
-            }}
-          >
-            <TodoContainer>
-              <CompletedBox />
-              <ScheduleName>{todo}</ScheduleName>
-            </TodoContainer>
-          </Pressable>
+        todos.map((todo,index) => (
+          <TodoContainer>
+            <BouncyCheckbox
+              text={todo}
+            />
+            <Pressable 
+              onPress={()=>{
+                console.log("icon pressed",todo,index)
+                setTodos(prev=>prev.filter(item=>item!==todo))
+              }}
+            >
+              <Ionicons
+                style={{
+                  marginLeft:30,
+                }}
+                name="trash-outline"
+                size={15}
+                color="green"
+              />
+            </Pressable>
+          </TodoContainer>
         ))
+
       }
-      {/* TEXT_INPUT을 써볼거에오 */}
-      <NewTodoInput
-        onChangeText={(text)=>{
-          setNewTodo(text)
-        }}
-        onSubmitEditing={()=>{
-          //새ㅔ로운 일정 추가
-          setTodos((prev) => [...prev, newTodo]);
-          //빈칸으로 초기화
-          setNewTodo("")
 
-          // console.log(newTodo)
-        }}
-        placeholder="어떤 일을 하실건가요?"
-        value={newTodo}
-        // keyboardType="number-pad"
-        returnKeyType="done"
+      {
+        // 삼항연산자 if (조건){조건이 참일때 수행할 코드} else {조건이 참이 아닐때 수행할 코드}
+        //(조건)?(조건이참일때 수행할 코드):(조건이 참이 아닐때 수행할 코드)
 
-      />
+        //조건: inputShown이 true인가?
+        // inputShown===true?(
+        inputShown?(
+          //inputShown이 true일때
+          <NewTodoInput
+            onChangeText={(text)=>{
+              setNewTodo(text)
+            }}
+            onSubmitEditing={()=>{
+              //새ㅔ로운 일정 추가
+              setTodos((prev) => [...prev, newTodo]);
+              //빈칸으로 초기화
+              setNewTodo("")
+              setInputShown(false)
 
-      {/* <TextInput 
-      /> */}
+              // console.log(newTodo)
+            }}
+            placeholder="어떤 일을 하실건가요?"
+            value={newTodo}
+            // keyboardType="number-pad"
+            returnKeyType="done"
+
+          />
+        ):(
+          //inputShown이 true가 아닐때
+          null
+        )
+      }
     </>
   );
 };
